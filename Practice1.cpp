9a7ae1bc991a648ca47011499156be6d5173d3cd
VIVEK KUMAR SINGH <67855681+viveks4056@users.noreply.github.com>
2020-09-15 11:13:41 +0530

    Update Practice1.cpp

--- Practice1.cpp
+++ Practice1.cpp
@@ -1,1 +1,31 @@
-
+#include<iostream>
+using namespace std;
+//sorting an array in ascending and descending order
+void desArr(int arr[], int size)
+{    
+    for(int i = 0; i<size; i++)
+         cout<<arr[i]<<" ";
+    cout<<endl;
+    int temp = 0;
+    for(int i = 0; i<size; i++)
+    {
+        for(int j = i+1; j<size; j++)
+        {
+            if(arr[i]<arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+             }
+         }
+        cout<<arr[i]<<" ";
+     }
+}
+int main()
+{   
+    int size;
+    int arr[] = {4,3,6,8,10,3,3,4};
+    size = sizeof(arr)/sizeof(arr[0]);
+    desArr(arr, size);
+    return 0;
+ }
